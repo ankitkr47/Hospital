6011e74f8c3f0d40c263dfc7979b70679040b8b1
ankitkr47 <ankitkumardavmuz@gmail.com>
2023-05-19 02:06:19 +0530

    added ui

---
+++ script.js
@@ -0,0 +1,9 @@
+function initMap() {
+    new google.maps.Map(document.getElementById("map"), {
+      mapId: "ee3fb3ecdd9592ed",
+      center: { lat: 28.462876, lng: 77.490815 },
+      zoom: 14,
+    });
+  }
+  
+window.initMap = initMap;
