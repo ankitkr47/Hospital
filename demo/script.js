571cee0e1028deedfcf947f0cb6a416d93916bd5
Ankit Kumar <88185950+ankitkr47@users.noreply.github.com>
2023-06-22 15:34:54 +0530

    Delete demo directory

--- demo/script.js
+++
@@ -1,9 +0,0 @@
-function initMap() {
-  new google.maps.Map(document.getElementById("map"), {
-    mapId: "ee3fb3ecdd9592ed",
-    center: { lat: 28.462876, lng: 77.490815 },
-    zoom: 14,
-  });
-}
-
-window.initMap = initMap;
